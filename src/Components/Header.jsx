4cc905540ac2a1496a9dfe1b7e46536b42156729
KarolKotomski <karol.kotomski88@gmail.com>
2023-09-02 10:20:30 +0200

    Header component created and styled

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import "../Styles/Header.css";
-
+import logo from "../images/netflix_logo.png";
 
 const Header = () => {
-	return <div className="header">Header</div>;
+	return (
+		<div className='header'>
+			<div className='container'>
+				<div className='header-container'>
+					<img src={logo} alt='netflix logo' />
+					<button>Sign In</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
