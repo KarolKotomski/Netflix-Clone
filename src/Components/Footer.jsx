efe819a2c321ef1b4d24ef8b2f15cfbaacd70460
KarolKotomski <karol.kotomski88@gmail.com>
2023-09-05 17:24:08 +0200

    Footer element implemented

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+	return (
+		<footer className='footer'>
+			<div className='container'>
+				<div className='footer_container'>
+					<div className='title'>
+						<p>Questions? Contact us.</p>
+					</div>
+          <div className="list">
+          <ul>
+						<li>FAQ</li>
+						<li>Help Center</li>
+						<li>Account</li>
+						<li>Media Center</li>
+						<li>Investor Relations</li>
+						<li>Jobs</li>
+						<li>Redeem Gift Cards</li>
+						<li>Buy Gift Cards</li>
+						<li>Ways to Watch</li>
+						<li>Terms of Use</li>
+						<li>Privacy</li>
+						<li>Cookie Preferences</li>
+						<li>Corporate Information</li>
+						<li>Contact Us</li>
+						<li>Speed Test</li>
+						<li>Legal Guarantee</li>
+						<li>Legal Notices</li>
+						<li>Only on Netflix</li>
+						<li>Ad Choices</li>
+					</ul>
+          </div>
+				</div>
+			</div>
+		</footer>
+	);
+};
 
-export default Footer
+export default Footer;
