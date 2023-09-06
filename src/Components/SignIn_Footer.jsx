d4c2f432c8ca671af2dd041cadf43dfefc5dfb31
KarolKotomski <karol.kotomski88@gmail.com>
2023-09-06 13:08:21 +0200

    SignIn_Footer element styled

--- src/Components/SignIn_Footer.jsx
+++ src/Components/SignIn_Footer.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const SignIn_Footer = () => {
   return (
-    <div>SignIn_Footer</div>
+    <footer className='signIn_footer'>
+    <div className='container'>
+      <div className='signIn_footer_container'>
+        <div className='title'>
+          <p>Questions? Contact us.</p>
+        </div>
+        <div className="list">
+        <ul>
+          <li>FAQ</li>
+          <li>Help Center</li>
+          <li>Terms of Use</li>
+          <li>Privacy</li>
+          <li>Cookie Preferences</li>
+          <li>Corporate Information</li>
+          <li>Ad Choices</li>
+        </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
   )
 }
 
