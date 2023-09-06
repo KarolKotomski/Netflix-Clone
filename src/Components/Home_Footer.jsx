b13c9e96b9094a1823632b56ccc38ba75da97b38
KarolKotomski <karol.kotomski88@gmail.com>
2023-09-06 17:41:03 +0200

    Home page elements created. Home_Header component styled

---
+++ src/Components/Home_Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home_Footer = () => {
+  return (
+    <div>Home_Footer</div>
+  )
+}
+
+export default Home_Footer
---
+++ src/Components/Home_Header.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import logo from "../images/netflix_logo.png";
+import avatar from "../icons/avatar.jpg";
+import bell from "../icons/bell.png";
+import search from "../icons/search.png";
+
+const Home_Header = () => {
+	return (
+		<header className='home_header'>
+			<div className='container'>
+				<div className='home_header_container'>
+					<div className='bar'>
+						<img src={logo} alt='netflix logo' />
+						<div className='list'>
+							<ul>
+								<li>Home</li>
+								<li>TV Shows</li>
+								<li>Movies</li>
+								<li>New & Popular</li>
+								<li>My List</li>
+							</ul>
+						</div>
+					</div>
+					<div className='user_panel'>
+						<img src={search} alt='search' />
+						<img src={bell} alt='notifications bell' />
+						<img className="avatar" src={avatar} alt='profile icon' />
+					</div>
+				</div>
+			</div>
+		</header>
+	);
+};
+
+export default Home_Header;
