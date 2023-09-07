ffc41ed60ff22146b5f2bd474b52103d0f60f779
KarolKotomski <karol.kotomski88@gmail.com>
2023-09-07 13:35:43 +0200

    Home_Banner title and description implemented from API

--- src/Components/Home_Banner.jsx
+++ src/Components/Home_Banner.jsx
@@ -30,21 +30,11 @@ const Home_Banner = () => {
 			<div className='container_long'>
 				<div className='home_header_container_long'>
 					<div className='content'>
-						<h1 className='title'>Movie title</h1>
+                        <h1 className='title'>{movie?.title || movie?.name || movie?.original_name}</h1>
 						<button className='play_button'>Play</button>
 						<button className='more_info_button'>More Info</button>
 						<p className='description'>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel
-							quaerat nam error vitae hic. Consequuntur illo minima, maxime
-							quisquam quos enim perferendis dolore neque dolorum in? Eum fugiat
-							delectus nam. Lorem ipsum dolor sit amet, consectetur adipisicing
-							elit. Reprehenderit similique debitis iure, doloremque maxime
-							suscipit voluptas itaque corporis enim dolorem saepe, ipsum amet
-							quibusdam animi placeat dolorum necessitatibus vel aspernatur?
-							Lorem ipsum dolor sit amet consectetur adipisicing elit.
-							Praesentium mollitia sequi numquam repellendus consectetur
-							expedita aperiam itaque alias commodi cupiditate vel blanditiis,
-							repellat maiores eligendi libero natus voluptatem impedit nihil.
+							{movie?.overview}
 						</p>
 					</div>
 				</div>
