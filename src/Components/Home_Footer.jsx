b71d4286849602c1f5b93bc8267b12fd76d1a0bf
KarolKotomski <karol.kotomski88@gmail.com>
2023-09-07 10:07:14 +0200

    Social media icons added. Home_Footer component styled

--- src/Components/Home_Footer.jsx
+++ src/Components/Home_Footer.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import facebook from "../icons/fb_icon.png";
+import instagram from "../icons/insta_icon.png";
+import twitter from "../icons/twit_icon.png";
+import youtube from "../icons/yt_icon.png";
 
 const Home_Footer = () => {
-  return (
-    <div>Home_Footer</div>
-  )
-}
+	return (
+		<div className='home_footer'>
+			<div className='container_long'>
+        <div className='home_footer_container_long'>
+          <div className="icons">
+            <img src={facebook} alt="facebook icon" />
+            <img src={instagram} alt="instagram icon" />
+            <img src={twitter} alt="twitter icon" />
+            <img src={youtube} alt="youtube icon" />
+          </div>
+					<div className='list'>
+						<ul>
+							<li>Audio description</li>
+							<li>Help Center</li>
+							<li>Gift Cards</li>
+							<li>Media Center</li>
+							<li>Investor Relations</li>
+							<li>Jobs</li>
+							<li>Terms</li>
+							<li>Privacy</li>
+							<li>Legal Notices</li>
+							<li>Cookie Preferences</li>
+							<li>Corporate Information</li>
+							<li>Contact Us</li>
+							<li>Ad Choices</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default Home_Footer
+export default Home_Footer;
