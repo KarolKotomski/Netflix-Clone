eda9bed60255e1f4194d5a6779ede674ec85953c
KarolKotomski <karol.kotomski88@gmail.com>
2023-09-08 19:50:24 +0200

    HomeRow component styled. Black color changed back again to light-black.

--- src/Components/HomeRow.jsx
+++ src/Components/HomeRow.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "../axios/axios";
 
-const HomeRow = () => {
+const HomeRow = ({ title, fetchUrl, isLargeRow = false }) => {
+	const [movies, setMovies] = useState([]);
+
+	const base_url = "https://image.tmdb.org/t/p/original/";
+
+	useEffect(() => {
+		async function fetchData() {
+			const request = await axios.get(fetchUrl);
+			setMovies(request.data.results);
+			return request;
+		}
+		fetchData();
+	}, [fetchUrl]);
+
 	return (
 		<main className='home_row'>
-			<div className='container_long'>
-				<div className='home_row_container_long'>
-					<h2>Title</h2>
-				</div>
+			<div className='title'>
+				<h2>{title}</h2>
+			</div>
+			<div className='posters'>
+				{movies.map((movie) => (
+					<img
+						className={`poster ${isLargeRow && "poster_large"}`}
+						src={`${base_url}${
+							isLargeRow ? movie.poster_path : movie.backdrop_path
+						}`}
+						alt={movie.name}
+						key={movie.id}></img>
+				))}
 			</div>
 		</main>
 	);
