01bb6dc77cc4620baaacf25dafe6c616480146bd
KarolKotomski <karol.kotomski88@gmail.com>
2023-09-09 10:53:56 +0200

    Header transition effect added

--- src/Components/HomeHeader.jsx
+++ src/Components/HomeHeader.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../images/netflix_logo.png";
 import avatar from "../icons/avatar.jpg";
 import bell from "../icons/bell.png";
 import search from "../icons/search.png";
 
 const HomeHeader = () => {
+	const [show, handleShow] = useState(false);
+
+	const transitionHeader = () => {
+		if (window.scrollY > 100) {
+			handleShow(true);
+		} else {
+			handleShow(false);
+		}
+	};
+
+	useEffect(() => {
+		window.addEventListener("scroll", transitionHeader);
+		return () => {
+			window.removeEventListener("scroll", transitionHeader);
+		};
+	}, []);
+
 	return (
-		<header className='home_header'>
-			<div className='container_long'>
+		<header className={`home_header ${show && "black_background"}`}>
+			<div className="container_long">
 				<div className='home_header_container_long'>
 					<div className='bar'>
 						<img src={logo} alt='netflix logo' />
@@ -24,7 +41,7 @@ const HomeHeader = () => {
 					<div className='user_panel'>
 						<img src={search} alt='search' />
 						<img src={bell} alt='notifications bell' />
-						<img className="avatar" src={avatar} alt='profile icon' />
+						<img className='avatar' src={avatar} alt='profile icon' />
 					</div>
 				</div>
 			</div>
