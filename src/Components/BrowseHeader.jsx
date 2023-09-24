978a7864e0579ab816c66106fd90fdf1ee2c68c9
KarolKotomski <karol.kotomski88@gmail.com>
2023-09-24 15:10:51 +0200

    search field funcionality upgraded

--- src/Components/BrowseHeader.jsx
+++ src/Components/BrowseHeader.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState, useContext, useRef } from "react";
 import avatar from "../icons/avatar.jpg";
 import bell from "../icons/bell.png";
 import caret from "../icons/caret.png";
@@ -12,21 +12,37 @@ import { MoviesContext } from "../context/MoviesContext";
 const BrowseHeader = () => {
 	const { query, setQuery, handleSearch } = useContext(MoviesContext);
 	const [show, handleShow] = useState(false);
-	const [isActive, setIsActive] = useState(false);
+	const [isDropActive, setIsDropActive] = useState(false);
+	const [isSearchActive, setIsSearchActive] = useState(false);
+
+	const searchInputRef = useRef(null);
+	const searchInputfield = useRef(null);
 
 	const handleClick = () => {
 		window.scrollTo(0, 0);
 		setQuery("");
 	};
 
-	const handleOpenMenu = () => {
-		setIsActive(true);
+	// Search Menu:
+	const handleOpenSearch = () => {
+		setIsSearchActive(true);
+		searchInputfield.current.focus();
 	};
 
-	const handleCloseMenu = () => {
-		setIsActive(false);
+	const handleCloseSearch = () => {
+		setIsSearchActive(false);
 	};
 
+	// Dropdown Menu:
+	const handleOpenDrop = () => {
+		setIsDropActive(true);
+	};
+
+	const handleCloseDrop = () => {
+		setIsDropActive(false);
+	};
+
+	// Transition Header:
 	const transitionHeader = () => {
 		if (window.scrollY > 100) {
 			handleShow(true);
@@ -39,6 +55,21 @@ const BrowseHeader = () => {
 		window.addEventListener("scroll", transitionHeader);
 		return () => {
 			window.removeEventListener("scroll", transitionHeader);
+		};
+	}, []);
+
+	// When clicked outside the input:
+	const handleOutsideClick = (e) => {
+		if (searchInputRef.current && !searchInputRef.current.contains(e.target)) {
+			handleCloseSearch();
+		}
+	};
+
+	useEffect(() => {
+		document.addEventListener("mousedown", handleOutsideClick);
+
+		return () => {
+			document.removeEventListener("mousedown", handleOutsideClick);
 		};
 	}, []);
 
@@ -65,37 +96,53 @@ const BrowseHeader = () => {
 						</div>
 					</div>
 					<div className='user_panel'>
-						<div className='search'>
-							<div className='search_bar'>
-								<div className='search_bar_wrapper_icon'>
-									<img src={search} alt='search' />
-								</div>
-								<div className='search_bar_wrapper_input_field'>
-									<input type='text' value={query} onChange={handleSearch} />
-								</div>
-								<div className="search_bar_wrapper_close">
-								<img src={close} alt='search' />
-								</div>
+						<div
+							className={isSearchActive ? "search_bar open" : "search_bar"}
+							ref={searchInputRef}>
+							<div className='search_bar_wrapper_icon'>
+								<img src={search} alt='search' />
 							</div>
-							<img src={search} alt='search' />
+							<div className='search_bar_wrapper_input_field'>
+								<input
+									type='text'
+									value={query}
+									onChange={handleSearch}
+									placeholder='Type your title'
+									ref={searchInputfield}
+								/>
+							</div>
+							<div
+								className='search_bar_wrapper_close'
+								onClick={() => setQuery("")}>
+								<img
+									className={query ? "visible" : "invisible"}
+									src={close}
+									alt='search'
+								/>
+							</div>
 						</div>
+						{!isSearchActive && (
+							<div className='search' onClick={() => handleOpenSearch()}>
+								<img src={search} alt='search' />
+							</div>
+						)}
 						<div className='bell'>
 							<img src={bell} alt='notifications bell' />
 						</div>
 						<div
 							className='profile'
-							onMouseOver={() => handleOpenMenu()}
-							onMouseLeave={() => handleCloseMenu()}>
+							onMouseOver={() => handleOpenDrop()}
+							onMouseLeave={() => handleCloseDrop()}>
 							<div className='avatar'>
 								<img src={avatar} alt='profile icon' />
-								<div className={`arrow ${isActive && "open"}`}>
+								<div className={`arrow ${isDropActive && "open"}`}>
 									<img src={caret} alt='caret icon'></img>
 								</div>
 							</div>
 							<div className='caret'>
 								<img src={caret} alt='caret icon' />
 							</div>
-							{isActive && <DropdownMenu />}
+							{isDropActive && <DropdownMenu />}
 						</div>
 					</div>
 				</div>
