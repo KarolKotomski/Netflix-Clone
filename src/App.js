64cab84373c645a6ec1e026e388bd92fdeb25048
KarolKotomski <karol.kotomski88@gmail.com>
2024-02-18 18:49:52 +0100

    lazy loading applied.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import "./dist/styles.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Browse from "./pages/Browse";
-import Landing from "./pages/Landing";
-import SignIn from "./pages/SignIn";
+import { SearchContextProvider } from "./context/SearchContext";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Suspense, lazy } from "react";
 import BrowseHome from "./components/BrowseHome";
 import BrowseTvShows from "./components/BrowseTvShows";
 import BrowseMovies from "./components/BrowseMovies";
-import NoMatch from "./components/NoMatch";
-import { SearchContextProvider } from "./context/SearchContext";
-import { QueryClient, QueryClientProvider } from "react-query";
 
 function App() {
 	const client = new QueryClient({
@@ -19,21 +16,28 @@ function App() {
 		},
 	});
 
+	const Landing = lazy(() => import("./pages/Landing"));
+	const SignIn = lazy(() => import("./pages/SignIn"));
+	const Browse = lazy(() => import("./pages/Browse"));
+	const NoMatch = lazy(() => import("./pages/NoMatch"));
+
 	return (
 		<QueryClientProvider client={client}>
 			<SearchContextProvider>
 				<BrowserRouter>
-					<Routes>
-						<Route path='/' element={<Landing />} />
-						<Route path='signin' element={<SignIn />} />
-						<Route path='browse' element={<Browse />}>
-							<Route index element={<BrowseHome />} />
-							<Route path='home' element={<BrowseHome />} />
-							<Route path='tvshows' element={<BrowseTvShows />} />
-							<Route path='movies' element={<BrowseMovies />} />
-						</Route>
-						<Route path='*' element={<NoMatch />} />
-					</Routes>
+					<Suspense fallback={<div>Loading...</div>}>
+						<Routes>
+							<Route path='/' element={<Landing />} />
+							<Route path='signin' element={<SignIn />} />
+							<Route path='browse' element={<Browse />}>
+								<Route index element={<BrowseHome />} />
+								<Route path='home' element={<BrowseHome />} />
+								<Route path='tvshows' element={<BrowseTvShows />} />
+								<Route path='movies' element={<BrowseMovies />} />
+							</Route>
+							<Route path='*' element={<NoMatch />} />
+						</Routes>
+					</Suspense>
 				</BrowserRouter>
 			</SearchContextProvider>
 		</QueryClientProvider>
